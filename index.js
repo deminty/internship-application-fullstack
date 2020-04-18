8baedc0bf53fb1f824e0144073b1bb6eafdbe1bd
deminty <31461024+deminty@users.noreply.github.com>
2020-04-18 10:59:20 -0500

    it is finished

--- index.js
+++ index.js
@@ -6,7 +6,42 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+  const urls = await fetch('https://cfw-takehome.developers.workers.dev/api/variants')
+    .then((response) => response.json())
+
+  const randomIndex = Math.round(Math.random())
+  const url = urls.variants[randomIndex]
+  const response = await fetch(url)
+  return new HTMLRewriter()
+    .on('title', new TitleHandler())
+    .on('h1#title', new TitleHandler2())
+    .on('p#description', new DescriptionHandler())
+    .on('a#url', new URLHandler())
+    .transform(response)
+}
+
+class TitleHandler {
+  element(element) {
+    return element.setInnerContent('My Application')
+  }
+}
+
+class TitleHandler2 {
+  element(element) {
+    return element.prepend('My Masterpiece: ')
+  }
+}
+
+class DescriptionHandler {
+  element(element) {
+    return element.setInnerContent('I did the thing.')
+  }
+}
+
+class URLHandler {
+  element(element) {
+    return element
+      .setInnerContent('Make your own Cloudflare Application!')
+      .setAttribute('href', 'https://github.com/cloudflare-internship-2020?tab=repositories')
+  }
 }
